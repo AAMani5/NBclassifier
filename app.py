32fec8508b6677e4d9894cb9b372c1f6e43d77d3
Ashwini <ashie.oh@gmail.com>
2017-04-25 14:46:50 +0100

    Combine headline and classification

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+from classifier import trainedNBClassifier, extract_features
 
 app = Flask(__name__)
 
@@ -11,12 +12,24 @@ def index():
 @app.route("/test", methods=['POST'])
 def test():
     if request.method == 'POST':
+        # text = request.form['userinput']
+        # print(text)
         r = requests.get("http://content.guardianapis.com/search?api-key=test")
         headlines = json.loads(r.text)['response']['results']
         # print(headlines)
-        text = request.form['userinput']
-        # print(text)
-    return render_template('test.html', text=text, headlines=headlines)
+        results = []
+        lines = []
+        for headline in headlines:
+            lines.append(headline["webTitle"])
+        print(lines)
+        for line in lines:
+            problemInstance = line.split()
+            problemFeatures = extract_features(problemInstance)
+            result = trainedNBClassifier.classify(problemFeatures)
+            results.append(result)
+        send = zip(lines, results)
+        print(send)
+    return render_template('test.html', send=send)
 
 if __name__ == '__main__':
     app.debug = True
---
+++ classifier.py
@@ -0,0 +1,83 @@
+import nltk
+
+
+
+positiveReviewsFileName = "./polaritydata/rt-polarity.pos"
+negativeReviewsFileName = "./polaritydata/rt-polarity.neg"
+
+with open(positiveReviewsFileName,'r') as f:
+    positiveReviews = f.readlines()
+
+with open(negativeReviewsFileName,'r') as f:
+    negativeReviews = f.readlines()
+
+
+testTrainingSplitIndex = 2500
+
+testNegativeReviews = negativeReviews[testTrainingSplitIndex+1:]
+testPositiveReviews = positiveReviews[testTrainingSplitIndex+1:]
+
+trainingNegativeReviews = negativeReviews[:testTrainingSplitIndex]
+trainingPositiveReviews = positiveReviews[:testTrainingSplitIndex]
+
+
+
+# def getTestReviewSentiments(naiveBayesSentimentCalculator):
+#   testNegResults = [naiveBayesSentimentCalculator(review) for review in testNegativeReviews]
+#   testPosResults = [naiveBayesSentimentCalculator(review) for review in testPositiveReviews]
+#   labelToNum = {'positive':1,'negative':-1}
+#   numericNegResults = [labelToNum[x] for x in testNegResults]
+#   numericPosResults = [labelToNum[x] for x in testPosResults]
+#   return {'results-on-positive':numericPosResults, 'results-on-negative':numericNegResults}
+#
+#
+# def runDiagnostics(reviewResult):
+#   positiveReviewsResult = reviewResult['results-on-positive']
+#   negativeReviewsResult = reviewResult['results-on-negative']
+#   numTruePositive = sum(x > 0 for x in positiveReviewsResult)
+#   numTrueNegative = sum(x < 0 for x in negativeReviewsResult)
+#   pctTruePositive = float(numTruePositive)/len(positiveReviewsResult)
+#   pctTrueNegative = float(numTrueNegative)/len(negativeReviewsResult)
+#   totalAccurate = numTruePositive + numTrueNegative
+#   total = len(positiveReviewsResult) + len(negativeReviewsResult)
+#   print "Accuracy on positive reviews = " +"%.2f" % (pctTruePositive*100) + "%"
+#   print "Accurance on negative reviews = " +"%.2f" % (pctTrueNegative*100) + "%"
+#   print "Overall accuracy = " + "%.2f" % (totalAccurate*100/total) + "%"
+#
+
+
+def getVocabulary():
+  positiveWordList = [word for line in trainingPositiveReviews for word in line.split()]
+  negativeWordList = [word for line in trainingNegativeReviews for word in line.split()]
+  allWordList = [item for sublist in [positiveWordList,negativeWordList] for item in sublist]
+  allWordSet = list(set(allWordList))
+  vocabulary = allWordSet
+  return vocabulary
+
+def getTrainingData():
+  negTaggedTrainingReviewList = [{'review':oneReview.split(),'label':'negative'} for oneReview in trainingNegativeReviews]
+  posTaggedTrainingReviewList = [{'review':oneReview.split(),'label':'positive'} for oneReview in trainingPositiveReviews]
+  fullTaggedTrainingData = [item for sublist in [negTaggedTrainingReviewList,posTaggedTrainingReviewList] for item in sublist]
+  trainingData = [(review['review'],review['label']) for review in fullTaggedTrainingData]
+  return trainingData
+
+
+
+
+def extract_features(review):
+  review_words=set(review)
+  features={} # Dictionary
+  for word in vocabulary: # Keys corresponding to the word in vocabulary
+      features[word]=(word in review_words) # Values either true or false,
+                            # Depending if the worst was present in the review
+  return features
+
+
+def getTrainedNaiveBayesClassifier(extract_features, trainingData):
+  trainingFeatures=nltk.classify.apply_features(extract_features, trainingData)
+  trainedNBClassifier=nltk.NaiveBayesClassifier.train(trainingFeatures) # Train the Classifier
+  return trainedNBClassifier
+
+vocabulary = getVocabulary()
+trainingData = getTrainingData()
+trainedNBClassifier = getTrainedNaiveBayesClassifier(extract_features,trainingData)
