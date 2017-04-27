2a44c7c40068eeb341e40046902f81d2e00fc531
Ashwini <ashie.oh@gmail.com>
2017-04-27 17:03:18 +0100

    Add another get route to render json object

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, session
 import requests
 import json
 import pickle
+from twitterAPI import getTweets
 
 
 f = open('twitter_classifier.pickle', 'rb')
@@ -26,15 +27,28 @@ def extract_features(review):
 def index():
     return render_template('index.html')
 
-@app.route("/test", methods=['POST'])
+@app.route("/json")
+def json():
+    results = session['results']
+    return jsonify(results=results)
+
+@app.route("/test", methods=['POST', 'GET'])
 def test():
     if request.method == 'POST':
         text = request.form['userinput']
-        problemInstance = text.split()
-        problemFeatures = extract_features(problemInstance)
-        result = trainedNBClassifier.classify(problemFeatures)
-    return render_template('test.html', text=text, result=result)
+        tweets = getTweets(text, "en", 100, "recent", "tweets.txt")
+        results = []
+        for tweet in tweets:
+            problemInstance = tweet.split()
+            problemFeatures = extract_features(problemInstance)
+            result = trainedNBClassifier.classify(problemFeatures)
+            results.append(result)
+        classifications = zip(tweets, results)
+        session['results'] = results
+        return render_template('test.html', classifications=classifications)
+
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run()
